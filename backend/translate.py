23bd69456c6d0a24a205a62eeaa163351a9f6d8e
ala2q6 <lxrbckl@protonmail.com>
2022-12-04 02:04:37 -0600

    introduction to translate algorithm

--- backend/translate.py
+++ backend/translate.py
@@ -18,29 +18,29 @@ def translateFunction(
 
     # <
     # <
-    f1 = lambda l, r : [(j % 38) for i in l for j in range((i - r[0]), (i + r[1]))]
-    f2 = lambda l : {i : l.count(i) for i in pGraph}
+    f1 = lambda l : [int(i) for i in l if (i)]
+    f2 = lambda l, r : [(j % 38) for i in l for j in range((int(i) - r[0]), (int(i) + r[1]))]
+    f3 = lambda l : {i : l.count(i) for i in pGraph}
 
-    # m = 37
-    # for i in range((m - 4), (m + 5)):
-    #
-    #   print(i % 38)
+    # >
 
+    translate = {}
+    for k1, v1 in pLoad.items():
 
-    # translate = {}
-    # for k, v in pLoad.items():
-    #
-    #     print(v)
-    #     print(len(v)) # remove
-    #
-    #     # v = [int(i) for i in v if (i != '')]
-    #     translate[k] = {
-    #
-    #         k : {
-    #
-    #             'x' : ,
-    #             'y' :
-    #
-    #         }
-    #
-    #     for k, v in pNeighbor.items()}
+        translate[k1] = []
+        for k2, v2 in pNeighbor.items():
+
+            a = f1(l = v1)
+            b = f2(l = a, r = v2)
+            c = f3(l = b)
+
+            var = f3(l = f2(l = f3(l = v1), r = v2))
+            translate[k1].append({
+
+                'name' : k2,
+                'x' : list(c.keys()),
+                'y' : list(c.values())
+
+            })
+
+    print(translate) # remove
