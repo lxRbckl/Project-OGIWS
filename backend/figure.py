02098c9fa82b6898c89d00cab0d8f56fb01080cb
ala2q6 <lxrbckl@protonmail.com>
2022-12-04 13:11:27 -0600

    progress on graph

--- backend/figure.py
+++ backend/figure.py
@@ -1,6 +1,9 @@
 # import <
 from dash import dcc
+from lxRbckl import jsonLoad
 import plotly.graph_objects as go
+
+from backend.resource import gDirectory
 
 # >
 
@@ -8,16 +11,28 @@ import plotly.graph_objects as go
 def figureFunction(
 
         k: str,
-        v: list
+        v: list,
+        pLimit = jsonLoad(pFile = f'{gDirectory}/backend/template/limit.json'),
+        pGraph = jsonLoad(pFile = f'{gDirectory}/backend/template/graph.json')
 
 ):
     '''  '''
+
+    [print(k, n['name'], n['y']) for n in v]
+    print(v[0]['x'])
+    print()
 
     figure = go.Figure(
 
         layout = dict(
 
-            paper_bgcolor = 'rgb(248, 240, 227)'
+            paper_bgcolor = 'rgb(248, 240, 227)',
+            margin = {
+
+                'l' : 1,
+                'r' : 1
+
+            }
 
         ),
         data = [
@@ -33,7 +48,19 @@ def figureFunction(
         for n in v]
 
     )
-    figure.update_layout(barmode = 'stack')
-    figure.update_layout(title_text = k)
+    figure.update_layout(
+
+        title = k,
+        barmode = 'stack',
+        xaxis = dict(
+
+            type = 'category',
+            tickvals = pGraph,
+            ticktext = pGraph,
+            tickmode = 'array'
+
+        )
+
+    )
 
     return dcc.Graph(figure = figure)
